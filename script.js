f8db02a6f3b0db85e01543af564fa1c32f1c949f
ko-chan2312 <59499167+ko-chan2312@users.noreply.github.com>
2020-02-18 23:45:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,5 +9,17 @@ $(function(){
         }
     });
 });
-
 });
+
+$(function(){
+    $('.header-right-iphone').click(function(){
+      var $list =$(this).find('.menu-list');
+      if($list.hasClass('open')){
+        $list.removeClass('open');
+        $list.slideUp();
+      }else{
+        $list.addClass('open');
+        $list.slideDown();
+      }
+    });
+  });
